d43449e12b944ae97d62f17d83bb27d1bb3a9a07
Nikolai Kozhekhov <ukaoneseven@gmail.com>
2021-11-30 21:53:25 +0300

    Minor bug fix

--- di/typescript-inversify/src/index.ts
+++ di/typescript-inversify/src/index.ts
@@ -1,13 +1,13 @@
 import { myContainer } from "./inversify.config";
 import { TYPES } from "./types";
-import {Dog} from './dog';
+import { Dog } from "./dog";
 
 class App {
-	createDog() {
-		const dog = myContainer.get<Dog>(TYPES.Dog);
-		dog.bark('bow-wow');
-	}
+  createDog() {
+    const dog = myContainer.get<Dog>(TYPES.Dog);
+    dog.bark("bow-wow");
+  }
 }
 
 const app = new App();
-dogService.createDog();
+app.createDog();
